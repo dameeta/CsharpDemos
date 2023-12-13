43f6363a85881b656532c21e759faaeb00be48b0
Dameeta <85731431+dameeta@users.noreply.github.com>
2023-12-13 12:52:29 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicDemo
+{
+     class Class1
+    {
+        public void class1Method()
+        {
+            Console.WriteLine("Class1Method");
+        }
+    }
+    class Class2 {
+
+        public void class2Method()
+        {
+            Console.WriteLine("Class2Method");
+        }
+    }
+    class Class3 { 
+    public static void Main1()
+        {
+            Class1 class1 = new Class1();
+            Class2 class2 = new Class2();
+            class1.class1Method();
+            class2.class2Method();
+        }
+    }
+}
---
+++ MethodOverloading.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicDemo
+{
+    internal class MethodOverloading
+    {
+        public int findArea(int l,int b)
+        {
+            return l * b;
+        }
+        public double findArea(double radius) {
+            return 3.14 * radius * radius;
+        }
+
+        public double findArea(float b,float h) {
+
+            return 0.5 * (b * h);
+
+        }
+        public double findArea(float side)
+        { 
+        
+        return side* side;
+        }
+
+        public static void Main(string[] args)
+        {
+
+MethodOverloading methodOverloading = new MethodOverloading();
+            Console.WriteLine("Area of Rectangle:"+ methodOverloading.findArea(4, 5));
+            Console.WriteLine("Area of Circle: "+ methodOverloading.findArea(3.4));
+            Console.WriteLine("Area of Triangle:"+ methodOverloading.findArea(2.3));
+            Console.WriteLine("Area of Square:" + methodOverloading.findArea(5.5));
+
+
+        }
+    }
+}
